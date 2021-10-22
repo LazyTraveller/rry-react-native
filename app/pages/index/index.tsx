ebaef0de13d17b16ecf949040bc382738353529e
heweizhi <heweizhi@guanmai.cn>
2021-10-22 16:25:38 +0800

    feat: add demo

---
+++ app/pages/index/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const Index = () => {
+  return (
+    <Text>this is a test demo </Text>
+  )
+}
+
+export default Index
